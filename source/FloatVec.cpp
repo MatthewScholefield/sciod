5f53a1c2fa33b805da58c68e82f3fa7dd96c951e
Matthew D. Scholefield <matthew3311999@gmail.com>
2017-04-19 18:21:37 -0500

    Add single floatvec constructor

--- source/FloatVec.cpp
+++ source/FloatVec.cpp
@@ -17,5 +17,10 @@
 
 #include "FloatVec.hpp"
 
+FloatVec FloatVecSingle(float a)
+{
+	return FloatVec(1, a);
+}
+
 FloatVecIO::FloatVecIO(const FloatVec& in, const FloatVec& out) :
 in(in), out(out) { }
--- source/FloatVec.hpp
+++ source/FloatVec.hpp
@@ -22,6 +22,8 @@
 using FloatVec = std::vector<float>;
 using FloatVec2D = std::vector<FloatVec>;
 
+FloatVec FloatVecSingle(float a);
+
 struct FloatVecIO
 {
 	FloatVecIO(const FloatVec &in, const FloatVec &out);
